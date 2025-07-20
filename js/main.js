de265f1127cbbaed7710fa59a8eae70809a25a06
mattleouf <96353353+mattleouf@users.noreply.github.com>
2025-07-20 15:38:58 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
+document.getElementById("play").addEventListener("click",
+  () => location.href = "game.html");     // to be built later
 
+document.getElementById("options").addEventListener("click",
+  () => location.href = "options.html");  // to be built later
